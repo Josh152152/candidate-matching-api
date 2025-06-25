de75f5c1c527045a4d1603184d720c9bbf7f9152
Josh152152 <sebastien.giard.fr@gmail.com>
2025-06-25 21:17:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -397,5 +397,5 @@ def link_profile():
         }), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 10000))  # Render sets PORT env
+    app.run(host='0.0.0.0', port=port)
